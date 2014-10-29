7117122ea3ae60b8ecc4a510024e9508cba7b7e4
galaxyrocker <davis.sandefur@gmail.com>
2014-10-29 17:56:37 -0500

    fixed irish_classes
    Something weird happened on last commit; fixed it

--- Irish-Dictionary-GUI/dictionary_classes.py
+++ Irish-Dictionary-GUI/dictionary_classes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Irish-Dictionary-GUI/dictionary_classes.py
 # Irish Dictionary Checker Modules
 # Saved as dictionary_classes.py
 # Created by Davis Sandefur; last updated 29.10.14
@@ -18,39 +17,7 @@ class WordLookup(object): # Create a class to look up the word
         self.word = urllib.parse.quote_plus(word)
         self.language = language
         
-=======
-# Irish Dictionary GUI
-# Saved as dictionary_gui.py
-# Last updated by: Davis Sandefur 26/10/14
 
-from Gui import *
-from irish_dictionary2 import *
-
-
-g = Gui()
-g.title("Irish Dictionary")
-wordlist=[]
-
-def callback1():
-    """Called when the user presses 'English'"""
-    entries, suggestions = irish_dictionary(entry.get(),'English')
-    for i in entries:
-        st.text.insert(END,i)
-        st.text.insert(END,'\n\n')
-    suggestions=str(suggestions[0])
-    suggestions = ' '.join(suggestions.split())
-    st.text.insert(END,"\n")
-    st.text.insert(END,suggestions)
-    wordlist.insert(0,entry.get())
-    if len(wordlist)>5:
-        wordlist.pop()
-    st.text.insert(END,"\n\nRecently used words: ")
-    st.text.insert(END,wordlist)
-    st.text.insert(END,"\n\n")
->>>>>>> origin/master:Irish-Dictionary-GUI/irish_dictionary2.py
-    
-
-<<<<<<< HEAD:Irish-Dictionary-GUI/dictionary_classes.py
 class HTMLRead(object):
     """ This class reads the HTML and gets the text in a list ready to be
     read by the program. """
@@ -77,36 +44,4 @@ class WordStore(object):
         if len(self.wordlist)> 5:
             self.wordlist.pop()
         return self.wordlist
-=======
-    
-        
-def callback2():
-    """Called when the user presses 'Irish'"""
-    entries, suggestions  = irish_dictionary(entry.get(),'Irish')
-    for i in entries:
-        st.text.insert(END,i)
-        st.text.insert(END,"\n\n")
-    suggestions=str(suggestions[0])
-    suggestions = ' '.join(suggestions.split())
-    st.text.insert(END,"\n")
-    st.text.insert(END,suggestions)
-    wordlist.insert(0,entry.get())
-    if len(wordlist)>5:
-        wordlist.pop()
-    st.text.insert(END,"\n\nRecently used words: ")
-    st.text.insert(END,wordlist)
-    st.text.insert(END,"\n\n")
-    
-
-
-
-g.gr(cols=1)
-entry = g.en()
-g.endgr()
-g.gr(cols=2)
-engbutton= g.bu(text='English', command=callback1)
-irishbutton= g.bu(text='Irish', command=callback2)
-g.endgr()
-
-st = g.st()
->>>>>>> origin/master:Irish-Dictionary-GUI/irish_dictionary2.py
+ 
