76b07a3113af33433c278b916072b6503e994d2c
galaxyrocker <davis.sandefur@gmail.com>
2014-11-01 00:02:15 -0500

    Updated dictionary_gui
    Dictionary_gui was updated to no longer require swampy.py, and instead
    rely solely on tkinter.

--- Irish-Dictionary-GUI/dictionary_gui.py
+++ Irish-Dictionary-GUI/dictionary_gui.py
@@ -1,62 +1,78 @@
 # Irish Dictionary GUI
 # Saved as dictionary_gui.py
-# Last updated by: Davis Sandefur 29/10/14
+# Last updated by: Davis Sandefur 31/10/14
 
-""" This program brings together irish_dictionary.py and GUI.py, to make a
-GUI that reports the entries for the word checked, suggestions, and a list,
-with a scrollable text bar and option of choosing either Irish or English."""
+from tkinter import *
+from irish_dictionary import irish_dictionary
 
-from Gui import *
-from irish_dictionary import *
+master = Tk()  # Create your Tk object
 
-
-g = Gui()
-g.title("Irish Dictionary")
+# Define callbacks
 
 
 def english_callback():
     """Called when the user presses 'English'"""
-    entries, suggestions, wordlist = irish_dictionary(entry.get(),'English')
+    entries, suggestions, wordlist = irish_dictionary(entry.get(), 'English')
     for i in entries:
-        st.text.insert(END,i)
-        st.text.insert(END,'\n\n')
-    suggestions=str(suggestions[0])
+        st.insert(END, i)
+        st.insert(END, '\n\n')
+    suggestions = str(suggestions[0])
     suggestions = ' '.join(suggestions.split())
-    st.text.insert(END,"\n")
-    st.text.insert(END,suggestions)
-    st.text.insert(END,"\n\nRecently used words: ")
-    st.text.insert(END,wordlist)
-    st.text.insert(END,"\n\n")
-    
+    st.insert(END, "\n")
+    st.insert(END, suggestions)
+    st.insert(END, "\n\nRecently used words: ")
+    st.insert(END, wordlist)
+    st.insert(END, "\n\n")
 
-    
-        
+
 def irish_callback():
     """Called when the user presses 'Irish'"""
-    entries, suggestions, wordlist  = irish_dictionary(entry.get(),'Irish')
+    entries, suggestions, wordlist = irish_dictionary(entry.get(), 'Irish')
     for i in entries:
-        st.text.insert(END,i)
-        st.text.insert(END,"\n\n")
-    suggestions=str(suggestions[0])
+        st.insert(END, i)
+        st.insert(END, "\n\n")
+    suggestions = str(suggestions[0])
     suggestions = ' '.join(suggestions.split())
-    st.text.insert(END,"\n")
-    st.text.insert(END,suggestions)
-    st.text.insert(END,"\n\nRecently used words: ")
-    st.text.insert(END,wordlist)
-    st.text.insert(END,"\n\n")
-    
+    st.insert(END, "\n")
+    st.insert(END, suggestions)
+    st.insert(END, "\n\nRecently used words: ")
+    st.insert(END, wordlist)
+    st.insert(END, "\n\n")
 
 
+# Create all widgets
 
-g.gr(cols=1)
-entry = g.en()
-g.endgr()
-g.gr(cols=2)
-engbutton= g.bu(text='English', command=english_callback)
-irishbutton= g.bu(text='Irish', command=irish_callback)
-g.endgr()
+# Create the first frame to hold the Label and entry widgets
 
-st = g.st()
+frame1 = Frame(master)
+frame1.pack()
+
+l = Label(frame1, text="Enter your word:")
+l.pack(side=LEFT, expand=True, ipadx=100)
+entry = Entry(frame1)
+entry.pack(side=RIGHT, expand=True, ipadx=122)
+
+# Create a second frame to hold all your buttons
+
+frame2 = Frame(master)
+frame2.pack()
+
+english_button = Button(frame2, text='English', command=english_callback)
+english_button.pack(side=LEFT, expand=True, ipadx=145)
+irish_button = Button(frame2, text='Irish', command=irish_callback)
+irish_button.pack(side=RIGHT, expand=True, ipadx=145)
 
 
+# Create the third frame to hold your scrollable text
 
+frame3 = Frame(master)
+frame3.pack()
+
+
+st = Text(frame3)
+scrollbar = Scrollbar(frame3)
+scrollbar.pack(side=RIGHT, fill=Y)
+scrollbar.config(command=st.yview)  # Linking scroll
+st.config(yscrollcommand=scrollbar.set)  # Linking scroll
+st.pack(side=LEFT, fill=BOTH, expand=1)
+master.mainloop()
