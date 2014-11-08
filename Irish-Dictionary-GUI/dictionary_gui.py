3b7c98684632783fe79adec783144dbddf69c975
galaxyrocker <davis.sandefur@gmail.com>
2014-11-07 18:21:13 -0600

    Multilingual Version 2.0 Fixes Issue ##1
    Worked out way to change interface language so it can be either English or Irish. Fixes issue #1

--- Irish-Dictionary-GUI/dictionary_gui.py
+++ Irish-Dictionary-GUI/dictionary_gui.py
@@ -1,14 +1,13 @@
 # Irish Dictionary GUI
 # Saved as dictionary_gui.py
-# Last updated by: Davis Sandefur 2/11/14
+# Last updated by: Davis Sandefur 7/11/14
 
+# TODO: Work on dimensions of expand
 
 from tkinter import *
 from irish_dictionary import irish_dictionary
 from dictionary_classes import StringCleanup
 
-
-# TODO: Add Irish Language Version
 
 class Callback:
 
@@ -38,43 +37,74 @@ class Callback:
         frame3.st.insert(END, wordlist)
         frame3.st.insert(END, "\n\n")
 
+    def english_to_irish():
+        """ This is called when the button to turn the interface to Irish is pressed"""
+        frame1.entry.pack_forget()  # Forget English entry
+        frame1.entry = Entry(frame1.irish_frame, text="Cuir d'fhocal anseo")  # Create new entry for Irish version
+        frame1.entry.pack(expand=True, fill=BOTH)
+        frame1.english_frame.pack_forget()  # Forget English version of input
+        frame1.irish_frame.pack(expand=True, fill=BOTH)
+        frame2.english_frame.pack_forget()  # Forget the English buttons
+        frame2.irish_frame.pack(expand=True, fill=BOTH)
+        frame3.frame3.pack_forget()  # Remove scrollable text from top
+        frame3.frame3.pack(expand=True, fill=BOTH)  # Repack scrollable text at bottom
+
+    def irish_to_english():
+        """ This is called when th button to turn the interface to English is pressed """
+        frame1.entry.pack_forget()  # Forget the Irish entry
+        frame1.entry = Entry(frame1.english_frame, text="Enter your word here")  # Create new entry with English version
+        frame1.entry.pack(expand=True, fill=BOTH)
+        frame1.irish_frame.pack_forget()  # Forget the rest of the Irish input version
+        frame1.english_frame.pack(expand=True, fill=BOTH)
+        frame2.irish_frame.pack_forget()  # Forget the Irish buttons
+        frame2.english_frame.pack(expand=True, fill=BOTH)
+        frame3.frame3.pack_forget()  # Remove scrollable text from top
+        frame3.frame3.pack(expand=True, fill=BOTH)  # Repack scrollable text at bottom
+
 
 class FirstFrame(object):
-    def __init__(self):
-        self.frame1 = Frame()
-        self.frame1.pack()
-        self.entry = Entry(self.frame1)
+    def __init__(self, root):
+        # Create a frame for the English version
+        self.english_frame = Frame(root)
+        self.english_frame.pack(expand=True, fill=BOTH)  # English is default version
+        self.entry = Entry(self.english_frame)
+        english_label = Label(self.english_frame, text="Enter your word:")
+        english_label.pack(side=LEFT, expand=True, ipadx=50, fill=BOTH)
+        english_language_button = Button(self.english_frame, text="Leagan Gaeilge", command=Callback.english_to_irish)
+        english_language_button.pack(side=RIGHT, padx=50, fill=BOTH)
 
-        if language.language == 'English':
-            l = Label(self.frame1, text="Enter your word:")
-            l.pack(side=LEFT, expand=True, ipadx=50)
-            language_button = Button(self.frame1, text="Leagan Gaeilge", command=language.english_to_irish)
-            language_button.pack(side=RIGHT, padx=50)
-
-
-        if language.language == 'Irish':
-            l = Label(self.frame1, text="Cuir d'fhocal anseo")
-            l.pack(side=LEFT, expand=True, ipadx=50)
-            language_button = Button(self.frame1, text="English Version", command=language.irish_to_english)
-            language_button.pack(side=RIGHT, padx=50)
-
-        self.entry.pack(expand=True, ipadx=50)
+        # Create a frame for the Irish version
+        self.irish_frame = Frame(root)
+        irish_label = Label(self.irish_frame, text="Cuir d'fhocal anseo")
+        irish_label.pack(side=LEFT, expand=True, ipadx=50, fill=BOTH)
+        irish_language_button = Button(self.irish_frame, text="English Version", command=Callback.irish_to_english)
+        irish_language_button.pack(side=RIGHT, padx=50, fill=BOTH)
+        self.entry.pack(expand=True, ipadx=50, fill=BOTH)  # Pack entry at the very end
 
 
-class Buttons():
-    def __init__(self):
-        self.frame2 = Frame()
-        self.frame2.pack()
-        english_button = Button(self.frame2, text='English', command=Callback.english_callback)
-        english_button.pack(side=LEFT, expand=True, ipadx=145)
-        irish_button = Button(self.frame2, text='Irish', command=Callback.irish_callback)
-        irish_button.pack(side=RIGHT, expand=True, ipadx=145)
+class Buttons(object):
+    def __init__(self, root):
+
+        # Create a frame for the English language buttons
+        self.english_frame = Frame(root)
+        self.english_frame.pack(expand=True, fill=BOTH)
+        english_button = Button(self.english_frame, text='English', command=Callback.english_callback)
+        english_button.pack(side=LEFT, expand=True, ipadx=145, fill=BOTH)
+        irish_button = Button(self.english_frame, text='Irish', command=Callback.irish_callback)
+        irish_button.pack(side=RIGHT, expand=True, ipadx=145, fill=BOTH)
+
+        # Create a second frame to hold the Irish language buttons
+        self.irish_frame = Frame(root)
+        bearla_button = Button(self.irish_frame, text='Béarla', command=Callback.english_callback)
+        bearla_button.pack(side=LEFT, expand=True, ipadx=145, fill=BOTH)
+        gaeilge_button = Button(self.irish_frame, text='Gaeilge', command=Callback.irish_callback)
+        gaeilge_button.pack(side=RIGHT, expand=True, ipadx=145, fill=BOTH)
 
 
-class ScrollText():
-    def __init__(self):
-        self.frame3 = Frame()
-        self.frame3.pack()
+class ScrollText(object):
+    def __init__(self, root):
+        self.frame3 = Frame(root)
+        self.frame3.pack(expand=True, fill=BOTH)
         self.st = Text(self.frame3)
         scrollbar = Scrollbar(self.frame3)
         scrollbar.pack(side=RIGHT, fill=Y)
@@ -82,29 +112,10 @@ class ScrollText():
         self.st.config(yscrollcommand=scrollbar.set)  # Linking scroll
         self.st.pack(side=LEFT, fill=BOTH, expand=1)
 
-
-class LanguageSwitcher:
-    def __init__(self):
-        self.language = 'English'  # English is default language
-
-
-    def english_to_irish(self):
-        """ This callback changes language to Irish  """
-        self.language = 'Irish'
-        print("This is being used")
-
-    def irish_to_english(self):
-        """ This callback changes language to English """
-        self.language = 'English'
-        print("This is being used")
-
-
 master = Tk()  # Create your Tk object
 master.title("Irish Dictionary Searcher")
-language = LanguageSwitcher()
-frame1 = FirstFrame()
-frame2 = Buttons()
-frame3 = ScrollText()
-
+frame1 = FirstFrame(master)
+frame2 = Buttons(master)
+frame3 = ScrollText(master)
 
 master.mainloop()
