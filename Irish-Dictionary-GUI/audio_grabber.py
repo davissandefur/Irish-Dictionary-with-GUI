c924c4b24482bfebb0ad421978743dd25bcad837
Davis Sandefur <davis.sandefur@gmail.com>
2014-12-16 21:53:46 -0600

    Updated audio_grabber.py to return related searches for a given word's sound option, if they exist. If it doesn't it returns an empty list. Still need to make audio download return the boolean for whether there's an entry for the word.

--- Irish-Dictionary-GUI/audio_grabber.py
+++ Irish-Dictionary-GUI/audio_grabber.py
@@ -1,21 +1,21 @@
 # This module will download the audio from breis.focloir.ie/fuaim if it exists
 # if not, it will return related matches to search, if they exist.
 # Saved as audio_catcher.py
-# Created by Davis Sandefur; last updated 13.12.14
+# Created by Davis Sandefur; last updated 16.12.14
 
 """ This module contains the code necessary to download the sound files for a particular word from breis.focloir.ie,
 assuming the files exist. It also returns related matches, if they exist. If there is no sound, it returns any related
 matches. If neither, it returns None
 """
 
-
+from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
 
 
 def entry_search(word):
-    """ This function downloads sound for the string <word> if it exists. It also returns related words from the webpage.
-    If word exists, it also returns TRUE. If words don't exist, it returns FALSE
+    """ This function downloads sound for the string <word> if it exists.  If word exists, it also returns TRUE. If words
+    don't exist, it returns FALSE
     """
     word = urllib.parse.quote_plus(word)
     for i in range(len(word)):
@@ -26,11 +26,35 @@ def entry_search(word):
 
     # print(word)  # Test to make sure word is in correct form for URL
 
-    slug_list = ['CanM%2f', 'CanC%2F', 'CanU%2F']
+    slug_list = ['CanM', 'CanC', 'CanU']
     for i in slug_list:
-        url = 'http://breis.focloir.ie/' + i + word + '.mp3'
+        url = 'http://breis.focloir.ie/' + i + '%2F' + word + '.mp3'
         print(url)  # Test to check URL
         urllib.request.urlretrieve(url, i+'.mp3')
+
+
+def related_matches(word):
+    """ This function returns the related matches of a string (word) as given by breis.focloir.ie/en/fuaim/<word>. It
+    returns them in list form, with nothing but spaces and the text.
+    """
+
+    word = urllib.parse.quote_plus(word)
+    for i in range(len(word)):
+        if word[i] == "+":
+            begin_word = word[:i]
+            end_word = word[i+1:]
+            word = begin_word + '_' + end_word
+    response = urllib.request.urlopen('http://breis.focloir.ie/en/fuaim/'+word)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    entry = soup.findAll('div', class_='partial')  # HTML of the entries
+    entries = []
+    for i in entry:
+        entries.append(i.get_text()[:-2])
+    return entries
+
+
+
 
 
 
@@ -38,3 +62,5 @@ def entry_search(word):
 if __name__ == '__main__':
     entry_search('téigh')
     entry_search('seo a chuaigh thart')
+    print(related_matches('téigh'))
+    print(related_matches('seo a chuaigh thart'))
