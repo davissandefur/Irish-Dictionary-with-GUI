f512d7ce0fede399448efeecd0639733685a9e49
Davis Sandefur <davis.sandefur@gmail.com>
2014-12-22 09:17:59 -0600

    Updated irish_dictionary.py to return "grammatical forms" if the option is present. If the website doesn't give it as a grammatical form of something, it returns None.

--- Irish-Dictionary-GUI/irish_dictionary.py
+++ Irish-Dictionary-GUI/irish_dictionary.py
@@ -9,26 +9,28 @@ the classes from the dictionary_functions.py folder to output the entries and
 suggestions in list format, ready to be read off"""
 
 
-def irish_dictionary(word, language, wordlist=[]):
+def irish_dictionary(word, language, version, wordlist=[]):
     """ This function checks breis.focloir.ie for a word, Irish or English, and
     returns the word and related words. If no word exists, it returns similar
     words as given by the website.  """
 
     # Gets the entry and suggestions
-    entry, suggestion = dictionary_functions.entry_lookup(word, language)
+    entry, suggestion, form_of = dictionary_functions.entry_lookup(word, language, version)
     
     # Get entries and suggestions in a list using HTMLRead class
     entries = dictionary_functions.entry_cleanup(entry)
     suggestions = dictionary_functions.entry_cleanup(suggestion)
     suggestions = dictionary_functions.string_cleanup(suggestions)
+    form_of = dictionary_functions.entry_cleanup(form_of)
+    form_of = dictionary_functions.string_cleanup(form_of)
     wordlist = dictionary_functions.word_store(word, wordlist)
 
-    return entries, suggestions, wordlist
+    return entries, suggestions, wordlist, form_of
 
 if __name__ == '__main__':
-    irish_entry, irish_suggestions, irish_wordlist = irish_dictionary('gnóthach', 'irish')
+    irish_entry, irish_suggestions, irish_wordlist, form_of = irish_dictionary('bí', 'irish', 'gaeilge')
     print(irish_wordlist)  # Prints before English word gets added
-    english_entry, english_suggestions, english_wordlist = irish_dictionary('hello', 'english')
+    english_entry, english_suggestions, english_wordlist, foo = irish_dictionary('hello', 'english', 'english')
     print(irish_entry)
     print()
     print(irish_suggestions)
